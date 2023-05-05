3cacf872132810c50dd738b28e1785c564d3e705
Angelo Ciffone <angelo.pcr@yahoo.com>
2023-05-04 23:16:31 -0400

    add Readme and some example fetch trys

---
+++ README.md
@@ -0,0 +1,49 @@
+Link to live site !!! https://aciffone23.github.io/Fantasy-Football-Stat-Tracker/
+
+Background
+
+The core functionality of this project revolves around tracking specific players' data and displaying their statistics in an intuitive pop-up window. Users can select their desired players, and the application will fetch the relevant data, presenting it in a visually engaging format. The interactive pop-up window showcases essential stats such as points scored, yards gained, touchdowns, and more, facilitating a deeper understanding of each player's contribution to their fantasy football team. By providing a comprehensive overview of individual player performance, this project empowers users to optimize their roster management and dominate their leagues.
+
+Functionality & MVPs
+Fill in this template:
+
+In { Fantasy Football Stat Tracker }, users will be able to:
+
+    * By default view all the players sorted by fantasy points (fetched from the Fantasy Football Data Pros api)
+    * Search a player using the search box and view the statistics of a given player in a tabular format 
+    * Filter players by position during a week or season
+        * Stretch goal: View player xtatistics color coded, the best stats of a player by red, green, yellow etc. 
+    * View more details of a player by clicking on the name of a player, opens up a pop up with more information on the player like their photo, team logo and individual stats
+        * Stretch Goal: weekly visualization graph of a player in a given position 
+
+WireFrame URL 
+
+    https://wireframe.cc/4OvGqE
+
+Technologies, Libraries, APIs
+    Fantasy Football Data Pros API - https://www.fantasyfootballdatapros.com/our_api
+    d3 api - https://github.com/d3/d3/blob/main/API.md
+
+Implementation Timeline:
+
+Friday Afternoon & Weekend:
+
+Set up the project structure, including the frontend framework and API integration
+Implement the default view of players sorted by fantasy points
+
+Monday:
+
+Add the search box functionality for finding players and displaying their stats in a tabular format
+Implement the filtering feature to allow users to filter players by position during a week or season
+
+Tuesday:
+
+Start developing the pop-up window to display more detailed information about a player, including their photo and team logo
+
+Wednesday:
+
+Work on the stretch goal of color-coding player statistics based on their performance
+Begin working on the stretch goal of displaying a weekly visualization graph for a player in a given position
+
+
+
--- src/scripts/example.js
+++ src/scripts/example.js
@@ -13,3 +13,106 @@ class Example {
 }
 
 export default Example;
+// fetching the data for the players 
+var playerList;
+
+function getPlayerNames() {
+  return new Promise((resolve, reject) => {
+    fetch('https://www.fantasyfootballdatapros.com/api/players/2019/1')
+      .then(response => response.json())
+      .then(data => {
+        if (!data) {
+          reject(new Error('Unable to fetch player data'));
+        }
+        playerList = data;
+        resolve(data);
+      })
+      .catch(error => reject(error));
+  });
+}
+
+getPlayerNames()
+
+// fetching stats for the player
+playerList.find(f => f.player_name === "Lamar Jackson").stats
+
+// fetching passing and passing yards for the player
+playerList.find(f => f.player_name === "Lamar Jackson").stats.passing.passing_yds
+
+
+playerList.sort(function(a, b){
+    return a.fantasy_points.ppr < b.fantasy_points.ppr ? 1 : a.fantasy_points.ppr > b.fantasy_points.ppr ? -1: 0;
+}).filter(f => f.position === "QB");
+
+
+    
+    
+    // sorting 
+    var dataSet =  playerList.sort(function(a, b){
+    return a.fantasy_points.ppr < b.fantasy_points.ppr ? 1 : a.fantasy_points.ppr > b.fantasy_points.ppr ? -1: 0;
+    }).filter(f => f.position === "QB").map(f => ({name: f.player_name, points: f.fantasy_points.ppr}))
+
+
+//     <div>
+//   <canvas id="myChart"></canvas>
+// </div>
+
+// <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+// <script>
+  const ctx = document.getElementById('myChart');
+
+
+
+var playerList;
+
+function getPlayerNames() {
+  return new Promise((resolve, reject) => {
+    fetch('https://www.fantasyfootballdatapros.com/api/players/2019/1')
+      .then(response => response.json())
+      .then(data => {
+        if (!data) {
+          reject(new Error('Unable to fetch player data'));
+        }
+        playerList = data;
+        resolve(data);
+      })
+      .then(function(){
+
+            // var dataSet = playerList.sort(function(a, b){
+            //     return a.fantasy_points.ppr < b.fantasy_points.ppr ? 1 : a.fantasy_points.ppr > b.fantasy_points.ppr ? -1: 0;
+            // }).filter(f => f.position === "QB");
+
+
+            var dataSet =  playerList.sort(function(a, b){
+            return a.fantasy_points.ppr < b.fantasy_points.ppr ? 1 : a.fantasy_points.ppr > b.fantasy_points.ppr ? -1: 0;
+            }).filter(f => f.position === "QB").map(f => ({name: f.player_name, points: f.fantasy_points.ppr}))
+            
+            new Chart(ctx, {
+                type: 'bar',
+                data: {
+                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                datasets: [{
+                    label: '# of Votes',
+                    data: dataSet,
+                    borderWidth: 1
+                }]
+                },
+                options: {
+                scales: {
+                    y: {
+                    beginAtZero: true
+                    }
+                }
+                }
+            });
+      })
+      .catch(error => reject(error));
+  });
+}
+
+getPlayerNames()
+
+
+// </script>
+    
