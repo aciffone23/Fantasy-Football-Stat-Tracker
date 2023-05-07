030de4f9caba0608828ded931d47341c5a28ff47
Angelo Ciffone <angelo.pcr@yahoo.com>
2023-05-07 00:48:25 -0400

    Update modal with search parameter instructions to reset search if needed

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -10,6 +10,7 @@ document.addEventListener('DOMContentLoaded', () => {
           <p>2. Use the filter dropdowns to get a search by week and position.</p>
           <p>3. Click the Filter button to see the results sorted by fantasy points.</p>
           <p>4. The results will be displayed below the filter dropdowns.</p>
+          <p>5. Clear the search text if needed to reset the search parameters.</p>
           <button id="close-modal" class="close">&times;</button>
         </div>
       `;
