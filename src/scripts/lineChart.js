bd72b3ad255cef990c86337f9013fe825f8002b4
Angelo Ciffone <angelo.pcr@yahoo.com>
2023-05-09 14:25:48 -0400

    changes

--- src/scripts/lineChart.js
+++ src/scripts/lineChart.js
@@ -7,56 +7,71 @@ export function lineChart(data, topPlayerData) {
     const height = 250 - margin.top - margin.bottom;
   
     const x = d3
-      .scaleLinear()
-      .range([0, width])
-      .domain(d3.extent(data, (d) => {
-        console.log(d.week,d.value);
-        return d.week;
+        .scaleLinear()
+        .range([0, width])
+        .domain(d3.extent(data, (d) => {
+            console.log(d.week,d.value);
+            return d.week;
     }));
 
     const y = d3
-      .scaleLinear()
-      .range([height, 0])
-      .domain([0, 55]);
+        .scaleLinear()
+        .range([height, 0])
+        .domain([0, 55]);
   
     const svg = d3
-      .select("#line-chart-container")
-      .append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-      .attr("transform", `translate(${margin.left},${margin.top})`);
+        .select("#line-chart-container")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", `translate(${margin.left},${margin.top})`);
     
      const line = d3
-      .line()
-      .x((d) => x(d.week))
-      .y((d) => y(d.value));
+        .line()
+        .x((d) => x(d.week))
+        .y((d) => y(d.value));
     
     svg
-      .append("path")
-      .datum(data)
-      .attr("fill", "none")
-      .attr("stroke", "red")
-      .attr("stroke-width", 2)
-      .attr("d", line);
+        .append("path")
+        .datum(data)
+        .attr("fill", "none")
+        .attr("stroke", "red")
+        .attr("stroke-width", 2)
+        .attr("d", line);
 
     const topPlayerLine = d3
-      .line()
-      .x((d) => x(d.week))
-      .y((d) => y(d.value));    
+        .line()
+        .x((d) => x(d.week))
+        .y((d) => y(d.value));    
 
     svg
-      .append("path")
-      .datum(topPlayerData)
-      .attr("fill", "none")
-      .attr("stroke", "blue")
-      .attr("stroke-width", 2)
-      .attr("d", topPlayerLine); 
+        .append("path")
+        .datum(topPlayerData)
+        .attr("fill", "none")
+        .attr("stroke", "blue")
+        .attr("stroke-width", 2)
+        .attr("d", topPlayerLine); 
+
+    svg
+        .append("text")
+        .attr("transform", `translate(${width / 2},${height + margin.bottom - 5})`)
+        .style("text-anchor", "middle")
+        .text("Week");
+
+    svg
+        .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - margin.left + 10)
+        .attr("x", 0 - height / 2)
+        .attr("dy", "1em")
+        .style("text-anchor", "middle")
+        .text("Fantasy Points");
   
     svg
-      .append("g")
-      .attr("transform", `translate(0,${height})`)
-      .call(d3.axisBottom(x));
+        .append("g")
+        .attr("transform", `translate(0,${height})`)
+        .call(d3.axisBottom(x));
   
     svg.append("g").call(d3.axisLeft(y));
   }
