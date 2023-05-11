d9881e7d7b66709ec57f50597ea8c06ca83ff0f1
Angelo Ciffone <angelo.pcr@yahoo.com>
2023-05-11 14:49:51 -0400

    minor spelling mistake fix

--- src/index.js
+++ src/index.js
@@ -96,7 +96,7 @@ function playerModal(player) {
     let categoryHTML = ''
     // currentName = player.player_name
     // const playerPhoto = `../imgs/playerPhotos/${player.player_name}.jpeg`
-    const playerPhoto = `https://aciffone23.github.io/Performance-Playbook/imgs/playerPhotos${(player.player_name)}.jpeg`;
+    const playerPhoto = `https://aciffone23.github.io/Performance-Playbook/imgs/playerPhotos/${(player.player_name)}.jpeg`;
     const teamLogoPath = `https://aciffone23.github.io/Performance-Playbook/imgs/${(player.team).toLowerCase()}.png`;
     const topPlayerPositionData = topPlayerWeeklyData.map((weekData) => {
       return {
